cf5424eac39307c36e2ef91b4d8be8ee50aac3b8
Jonathan Belyeu <jrbelyeu@gmail.com>
2020-06-12 10:42:21 -0600

    added example data and commands to generate figures

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# samplot_manuscript
-example data and commands for the figures in the samplot manuscript
+# Samplot manuscript figure plots and data
+Example data and commands for the figures in the samplot manuscript.
+
+## To re-generate figures:
+* Install samplot with `conda install -c bioconda samplot` (see https://github.com/ryanlayer/samplot)
+* Get a copy of the GRCh37 reference genome (I recommend using GGD, see below)
+* Edit the first line of the script `generate_figures.sh`. Replace `/uufs/chpc.utah.edu/common/HIPAA/u1072557/giab/ashkenazi_trio_dataset2/data/hs37d5.fa` with your own copy of the GRCh37 reference genome fasta
+* Run the `bash generate_figures.sh` script. Figures will overwrite images in the plots directories
+
+
+## GGD-managed reference genome
+* Install GGD (`conda install -c bioconda ggd`)
+* Install reference genome with ggd (`ggd install grch37-reference-genome-ensembl-v1`)
+* Activate the new environment variable, which will be used by the `generate_figures.sh` script (ggd will print that command at the end of the install sequence)
+* Continue with figure generation
+
+Note: If you prefer not to use GGD, you'll need to re-assign the reference genome variable `b37_ref` in `generate_figures.sh` to the location of your own copy of that reference
---
+++ generate_figures.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+set -e
+b37_ref=${ggd_grch37_reference_genome_ensembl_v1_file}
+hg002_illumina="data/HG002.hs37d5.300x.subset.bam"
+hg003="data/HG003.hs37d5.300x.subset.bam"
+hg004="data/HG004.hs37d5.300x.subset.bam"
+hg002_ont="data/HG002_ONTrel2_16x_RG_HP10xtrioRTG.subset.bam"
+hg002_pb="data/HG002_PB_70x_RG_HP10XtrioRTG.subset.bam"
+eiee_15_0022958="data/15-0022958.subset.bam"
+repeats="data/repeats_trf.bed.gz"
+genes="data/Homo_sapiens.GRCh37.87.sorted.gff3.gz"
+
+#figure 1
+samplot plot \
+    -c 1 -s 156526704 -e 156528936 \
+    -o plots/figure1/DEL_1_156526704_156528936.png\
+    -b \
+    $hg002_illumina\
+    $hg002_ont\
+    -n "Illumina (~300x 2x150bp)" "PacBio (~70X HP-partitioned)" \
+    -t DEL\
+    -A $repeats\
+    --annotation_filenames "Tandem Repeat Finder"\
+    --separate_mqual 10\
+    --start_ci 223,223 --end_ci 223,223 \
+    -T $genes\
+    --transcript_filename "GRCh37 Genes"
+
+#figure 2A
+samplot plot \
+    -c 4 -s 99813786 -e 99817098 \
+    -o plots/figure2/DUP_4_99813786_99817098.png\
+    -b \
+     $hg002_illumina\
+    -n "Illumina (~300x 2x150bp)" "ONT (~7x ultralong)"\
+    -t DUP -H 2
+
+#figure 2B
+samplot plot \
+    -c 12 -s 12544867 -e 12546613 \
+    -o plots/figure2/INV_12_12544867-12546613.png\
+    -b \
+     $hg002_illumina\
+    -n "Illumina (~300x 2x150bp)" \
+    -t INV -H 2
+
+#figure 2C
+samplot plot \
+    -c 2 -s 59405747 -e 59405747 \
+    -c X -s 151118513 -e 151118513 \
+    -o plots/figure2/CTX_2-X_59405747-151118513_59405747-151118513.png\
+    -b \
+     $eiee_15_0022958\
+    -n "Illumina (~30x 2x150bp)"\
+    -t CTX -H 2
+
+
+#supplemental figure 1A
+svviz $b37_ref\
+    1 156526704 156528936 \
+    -b $hg002_illumina\
+    -t 'del'\
+    -e plots/supplemental_figure1/DEL_1_156526704_156528936.svviz.svg\
+
+#supplemental figure 2A
+samplot plot \
+    -c 1 -s 156526704 -e 156528936 \
+    -o plots/supplemental_figure2/DEL_1_156526704_156528936_all_types.png\
+    -b \
+     $hg002_illumina\
+     $hg002_ont\
+     $hg002_pb\
+    -n "Illumina (~300x 2x150bp)" "ONT (~7x ultralong)" "PacBio (~70X HP-partitioned)"\
+    -t DEL
+
+#supplemental figure 2B
+samplot plot \
+    -c 4 -s 99813786 -e 99817098 \
+    -o plots/supplemental_figure2/DUP_4_99813786_99817098_all_types.png \
+    -b \
+     $hg002_illumina\
+     $hg002_ont\
+     $hg002_pb\
+    -n "Illumina (~300x 2x150bp)" "ONT (~7x ultralong)" "PacBio (~70X HP-partitioned)"\
+    -t DUP
+
+#supplemental figure 2C
+samplot plot \
+    -c 12 -s 12544867 -e 12546613 \
+    -o plots/supplemental_figure2/INV_12_12544867_12546613_all_types.png \
+    -b \
+     $hg002_illumina\
+     $hg002_ont\
+     $hg002_pb\
+    -n "Illumina (~300x 2x150bp)" "ONT (~7x ultralong)" "PacBio (~70X HP-partitioned)"\
+    -t INV
+
+#supplemental figure 2D
+samplot plot \
+    -c 1 -s 156532935 -e 156534935 \
+    -o plots/supplemental_figure2/1_156532935_156534935_all_types.png\
+    -b \
+     $hg002_illumina\
+     $hg002_ont\
+     $hg002_pb\
+    -n "Illumina (~300x 2x150bp)" "ONT (~7x ultralong)" "PacBio (~70X HP-partitioned)"
+
+#supplemental figure 3: trio
+samplot plot \
+    -c 1 -s 156526704 -e 156528936 \
+    -o plots/supplemental_figure3/DEL_1_156526704_156528936_trio.png\
+    -b \
+     $hg002_illumina\
+     $hg003\
+     $hg004\
+     -n "HG002 (child)" "HG003 (father)" "HG004 (mother)"\
+    -t DEL
+
+#supplemental figure 4A: svviz DUP
+svviz $b37_ref\
+    4 99813786 99817098 \
+    -b $hg002_illumina\
+    -b $hg002_pb\
+    -t 'insertion' \
+    -e plots/supplemental_figure4/DUP_4_99813786_99817098_all_types.svviz.svg\
+
+#supplemental figure 4B: svviz INV
+svviz $b37_ref\
+    12 12544867 12546613 \
+    -b $hg002_illumina\
+    -b $hg002_pb\
+    -t 'inv' \
+    -e plots/supplemental_figure4/INV_12_12544867_12546613_all_types.svviz.svg\
+
+#supplemental figure 4C: svviz no variant
+svviz $b37_ref\
+    1 156532935 156534935 \
+    -b $hg002_illumina\
+    -b $hg002_pb\
+    -t 'del' \
+    -e plots/supplemental_figure4/1_156532935_156534935_all_types.svviz.svg\
+
+#supplemental figure 4D: Illumina svviz trio del
+svviz $b37_ref\
+    1 156526704 156528936 \
+    -b $hg002_illumina\
+    -b $hg003\
+    -b $hg004\
+    -t 'del' \
+    -e plots/supplemental_figure4/trio_del_1_156526704_156528936_trio.svviz.svg\
